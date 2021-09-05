3908e35f95341e690294e5d4965461d3d9e58782
Jonathan Carroll Otsuka <djgoku@gmail.com>
2021-09-05 18:22:47 -0500

    Added a few more terraform outputs.

--- terraform/environment/output.tf
+++ terraform/environment/output.tf
@@ -9,3 +9,15 @@ output "ec2_id" {
 output "ec2_private_ip" {
   value = aws_instance.ec2-bastion.private_ip
 }
+
+output "es_endpoint" {
+  value = aws_elasticsearch_domain.poc.endpoint
+}
+
+output "es_kibana_endpoint" {
+  value = aws_elasticsearch_domain.poc.kibana_endpoint
+}
+
+output "ssh_command" {
+  value = "ssh -i ~/.ssh/id_rsa ec2-user@${aws_instance.ec2-bastion.id} -L 8080:${aws_instance.ec2-bastion.private_ip}:22 -L 9090:${aws_elasticsearch_domain.poc.endpoint}:443"
+}
