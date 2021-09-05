b8aca40e6cd1644c8d1f055eb843fe1125a0d36c
Jonathan Carroll Otsuka <djgoku@gmail.com>
2021-09-05 16:53:38 -0500

    Added ec2 information output.

--- terraform/environment/output.tf
+++ terraform/environment/output.tf
@@ -1,3 +1,11 @@
 output "lb_url" {
   value = aws_lb.nginx.dns_name
 }
+
+output "ec2_id" {
+  value = aws_instance.ec2-bastion.id
+}
+
+output "ec2_private_ip" {
+  value = aws_instance.ec2-bastion.private_ip
+}
